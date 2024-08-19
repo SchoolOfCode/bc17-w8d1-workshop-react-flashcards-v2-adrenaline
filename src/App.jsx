9285ca13d189f709e596b9134d91ba8ff5e51f50
Emilijxa <168300036+Emilijxa@users.noreply.github.com>
2024-08-19 14:48:20 +0200

    Added form

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import Header from './Components/Header/header.jsx'
 import Flashcards from './Components/Flashcards/flashcards.jsx'
 import Footer from './Components/Footer/footer.jsx'
+import Form from './Components/Form/form.jsx'
 
 function App() {
 
   return (
     <>
       <Header />
+      <Form />
       <Flashcards />
       <Footer />
     </>
